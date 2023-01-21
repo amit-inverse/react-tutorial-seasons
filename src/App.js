76bd85df121c2c6f779162ebc2d54fb91178aaaa
Amit Kumar Das <amit.inverse@gmail.com>
2023-01-21 20:57:12 +0530

    Getting a Users Physical Location

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 function App() {
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position),
+        (err) => console.log(err)
+    );
+
     return <div>App</div>;
 }
 
