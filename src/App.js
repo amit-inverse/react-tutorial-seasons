ae1e59c2fe2ea500949c4237a04283673fc6080e
Amit Kumar Das <amit.inverse@gmail.com>
2023-01-21 21:11:14 +0530

    Refactoring from Functional to Class Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-function App() {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
-    );
+import React from 'react';
 
-    return <div>App</div>;
+class App extends React.Component {
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (err) => console.log(err)
+        );
+
+        return <div>App</div>;
+    }
 }
 
 export default App;
