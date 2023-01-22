e4f6a688bb62a504930c68207b57d34ec31c8488
Amit Kumar Das <amit.inverse@gmail.com>
2023-01-22 10:04:00 +0530

    Determining Season

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,15 @@
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+};
+
 function SeasonDisplay({ lat }) {
-    console.log(lat);
+    const season = getSeason(lat, new Date().getMonth());
+
+    console.log(season);
 
     return <div>SeasonDisplay</div>;
 }
