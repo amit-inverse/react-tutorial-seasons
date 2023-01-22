5aea9278ba9d61d204a15e20dc5dc16d148ddf8d
Amit Kumar Das <amit.inverse@gmail.com>
2023-01-22 16:09:17 +0530

    Specifying Default Props

--- src/Spinner.js
+++ src/Spinner.js
@@ -1,9 +1,13 @@
-function Spinner() {
+function Spinner({ message }) {
     return (
         <div class="ui active dimmer">
-            <div class="ui text loader">Loading...</div>
+            <div class="ui text loader">{message}</div>
         </div>
     );
 }
+
+Spinner.defaultProps = {
+    message: 'Loading...',
+};
 
 export default Spinner;
