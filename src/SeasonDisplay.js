d03106e75b10148e162d1bcaeb17c74b4a66a4ee
Amit Kumar Das <amit.inverse@gmail.com>
2023-01-22 11:41:26 +0530

    Extracting Options to Coding Objects

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,3 +1,14 @@
+const seasonConfig = {
+    summer: {
+        text: "Let's hit the beach",
+        iconName: 'sun',
+    },
+    winter: {
+        text: "Burr, it's chilly",
+        iconName: 'snowflake',
+    },
+};
+
 const getSeason = (lat, month) => {
     if (month > 2 && month < 9) {
         return lat > 0 ? 'summer' : 'winter';
@@ -8,15 +19,13 @@ const getSeason = (lat, month) => {
 
 function SeasonDisplay({ lat }) {
     const season = getSeason(lat, new Date().getMonth());
-
-    const text = season === 'winter' ? "Burr, it's chilly" : "Let's hit the beach";
-    const icon = season === 'winter' ? 'snowflake' : 'sun';
+    const { text, iconName } = seasonConfig[season];
 
     return (
         <div>
-            <i className={`${icon} icon`}></i>
+            <i className={`${iconName} icon`}></i>
             <h1>{text}</h1>
-            <i className={`${icon} icon`}></i>
+            <i className={`${iconName} icon`}></i>
         </div>
     );
 }
