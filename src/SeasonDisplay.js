d036d4290592dbead59f25109454fa3d36c48eb9
Amit Kumar Das <amit.inverse@gmail.com>
2023-01-22 10:15:42 +0530

    Showing Icons

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -9,9 +9,14 @@ const getSeason = (lat, month) => {
 function SeasonDisplay({ lat }) {
     const season = getSeason(lat, new Date().getMonth());
 
+    const text = season === 'winter' ? "Burr, it's chilly" : "Let's hit the beach";
+    const icon = season === 'winter' ? 'snowflake' : 'sun';
+
     return (
         <div>
-            <h1>{season === 'winter' ? "Burr, it's chilly" : "Let's hit the beach"}</h1>
+            <i className={`${icon} icon`}></i>
+            <h1>{text}</h1>
+            <i className={`${icon} icon`}></i>
         </div>
     );
 }
