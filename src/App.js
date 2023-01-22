4ed111fdf422e3b5f68dcfc4f2bab5c1e4fd893d
Amit Kumar Das <amit.inverse@gmail.com>
2023-01-22 08:11:12 +0530

    Initializing State through Constructor

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null };
+    }
+
+    // React says we have to define render!
     render() {
         window.navigator.geolocation.getCurrentPosition(
             (position) => console.log(position),
             (err) => console.log(err)
         );
 
-        return <div>App</div>;
+        return <div>Latitude: </div>;
     }
 }
 
